558f3bbdd9fbb55b730489499d37bc0efcd65f3a
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-11-28 17:26:07 -0400

    Implemeting Authentication Manager and Bearer Token

---
+++ src/main/java/com/webflux/reactivesecurity/security/AuthenticationManager.java
@@ -0,0 +1,26 @@
+package com.webflux.reactivesecurity.security;
+
+import com.webflux.reactivesecurity.Exceptions.UnauthorizedException;
+import com.webflux.reactivesecurity.entity.UserEntity;
+import com.webflux.reactivesecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationManager implements ReactiveAuthenticationManager {
+    private final UserRepository userRepository;
+
+    @Override
+    public Mono<Authentication> authenticate(Authentication authentication) {
+        CustomPrincipal principal = (CustomPrincipal) authentication.getPrincipal();
+
+        return userRepository.findById(principal.getId())
+                .filter(UserEntity::isEnabled)
+                .switchIfEmpty(Mono.error(new UnauthorizedException("User disabled")))
+                .map(user -> authentication);
+    }
+}
