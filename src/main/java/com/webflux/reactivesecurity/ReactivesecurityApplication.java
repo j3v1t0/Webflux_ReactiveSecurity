96d033b57b57045a0a560346f962c69303bc9a32
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-11-28 11:26:53 -0400

    Uploading Configuration Files

---
+++ src/main/java/com/webflux/reactivesecurity/ReactivesecurityApplication.java
@@ -0,0 +1,13 @@
+package com.webflux.reactivesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivesecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactivesecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webflux/reactivesecurity/ReactivesecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflux.reactivesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivesecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
